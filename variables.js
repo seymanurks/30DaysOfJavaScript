16d7b444101f45be44c0bed3c7c4d123a5e0170c
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-02 17:35:12 +0300

    variables and datatypes

---
+++ variables.js
@@ -0,0 +1,14 @@
+let ad = "Seyma"
+let yas = 22 
+let ulke = "Turkiye"
+let evliMi = false
+
+console.log(ad, yas, ulke, evliMi)
+
+const YER_CEKIMI = 9.81
+const KAYNAMA_NOKTASI = 100
+const PI = 3.14
+
+console.log("Dünyanın yer cekimi", YER_CEKIMI, "'dir")
+console.log("Suyun kaynama noktası", KAYNAMA_NOKTASI, "derecedir")
+console.log("PI sayısı", PI, "dır")
