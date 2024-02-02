a44faa47b67c57cae97e519f378e99996b242053
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-02 20:33:50 +0300

    datatypes

---
+++ Day -2/data-types.js
@@ -0,0 +1,18 @@
+let word = "JavaScript"
+word[0] = "Y"
+
+console.log(word)
+
+let nums = [1, 2, 3]
+nums[0] = 0
+
+console.log(nums)
+
+let name = "Seyma"
+
+let fullName= `Benim adım ${name}`
+console.log(fullName)
+
+let a = 4, b = 5
+let sum = ` ${a} + ${b} = ${a+b}`
+console.log(sum)
