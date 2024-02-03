8d15dee9fa7e0f101c43cc0c37e4882e85e82544
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-03 22:53:00 +0300

    Add files via upload

---
+++ Day - 7/arrow-function.js
@@ -0,0 +1,10 @@
+function karislama(name) {
+    console.log(`Merhaba ${name}`)
+}
+karislama("Seyma")
+
+const karsilama = name => console.log(`Merhaba ${name}`)
+karsilama("Nur")
+
+const fullName = (name, surname) => console.log(`${name}, ${surname}`)
+fullName("Seyma", "K.")
