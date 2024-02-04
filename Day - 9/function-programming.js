d4c37bde4d81b28b2785662513b219cb1c4ee597
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-05 00:56:36 +0300

    sort

--- Day - 9/function-programming.js
+++ Day - 9/function-programming.js
@@ -7,7 +7,7 @@ let evenNumbers = []
   oddNumbers.forEach(convert) //forEach
   console.log(evenNumbers)*/
 
-const numbers = [1, 3, 5, 7, 9]
+/*const numbers = [1, 3, 5, 7, 9]
 
   const evenNumbers2 = numbers.map(number => number * 2) //map
   console.log(evenNumbers2)
@@ -25,4 +25,11 @@ const str = names.some((name) => typeof name === 'number') // yalnızca biri str
 console.log(str)
 
 console.log(numbers.find(number => number > 5)) // find
-console.log(numbers.findIndex(number => number > 5)) //findIndex
+console.log(numbers.findIndex(number => number > 5)) //findIndex*/
+
+const names = ["Aylin", "Zeynep", "Rüveyda", "Meltem", "Beyza"]
+console.log(names.sort()) //orijinal diziyi değiştirir
+
+const numbers = [71, 63, 97, 55, 2, 13]
+numbers.sort((a,b) => a - b)
+console.log(numbers)
