56e7923af6b5f098913271670c301e929cc62557
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-05 23:11:31 +0300

    Add files via upload

---
+++ Day - 13/consol-methods.js
@@ -0,0 +1,36 @@
+console.log("30 Days of JavaScript")
+
+console.log("Benim adım %s ve %d yaşındayım.", "Şeyma", 22)
+
+console.log("%cDUR!", "font-size: 30px; color: red")
+
+console.warn("Uyarı")
+
+console.error("Hata")
+
+let a = 5, b = 6
+console.assert(a > b, "a b'den büyük değil")
+
+let names = ["Ebrar", "Şeyma", "Merve"]
+console.table(names)
+
+const users = [
+    {
+      name: 'Şeyma',
+      title: 'Programmer',
+      age: 22
+    },
+    {
+      name: 'Sinem',
+      title: 'Teacher',
+      age: 25
+    },
+    {
+      name: 'Abdullah',
+      title: 'Developer',
+      age: 25
+    }
+]
+  console.table(users)
+
+
