2646c530606928c560a8cc328ae0db1f37607f26
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-06 20:50:59 +0300

    class

--- Day - 15/class.js
+++ Day - 15/class.js
@@ -1,11 +1,11 @@
 class Person {
-  constructor(name, age) {
+  constructor(name, age, gender) {
     this.name = name
     this.age = age
-    this.gender = "Kadın"
+    this.gender = gender
   }
   get getGender() {
-    return this.gender
+    return this.gender == 1 ? "Erkek" : "Kadın"
   }
   set setAge(age) {
     this.age = age
@@ -13,12 +13,16 @@ class Person {
   setAgeChange(age) {
     this.age = age
   }
+  static skills() {
+    const skills = ['HTML', 'CSS', 'JS', 'React', 'Python', 'Node']
+    return skills
+  }
 }
 
-const person = new Person("Şeyma", 22)
+const person = new Person("Şeyma", 22, 2)
 console.log(person) 
 
-const person2 = new Person("Hatice", 38)
+const person2 = new Person("Hatice", 38, 2)
 console.log(person2) 
 
 console.log(person.getGender)
@@ -28,3 +32,22 @@ console.log(person.age)
 
 person.setAgeChange(23)
 console.log(person.age)
+
+console.log(Person.skills())
+
+class Student extends Person {
+
+  constructor(name, age, gender, grade) {
+    super(name, age, gender)
+    this.grade = grade
+  }
+  learn() {
+    return "Dersi dinliyor"
+  }
+}
+
+const student = new Student("Nisa", 17, 2, 11)
+console.log(student.learn())
+
+console.log(student.age)
+console.log(student.grade)
