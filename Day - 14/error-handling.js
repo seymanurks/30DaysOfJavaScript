c9599a331ee2821c5350d87e6d3de52012a4b192
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-06 16:29:39 +0300

    try catch throw

--- Day - 14/error-handling.js
+++ Day - 14/error-handling.js
@@ -1,4 +1,4 @@
-try {
+/*try {
   let firstName = "Şeyma"
   let fullName = firstName + " " + lastName
 }
@@ -9,4 +9,29 @@ catch(err) {
 }
 finally {
   console.log('Her durumda çalıştırılacak kod')
+}*/
+
+try {
+  let firstName = "Şeyma"
+  let lastName = "K."
+  let fullName = firstName + " " + lastName
+  throw "throw ile hata fırlatıldı"
+}
+catch(err) {
+  console.log("Hatalı kullanım")
+}
+finally {
+  console.log('Her durumda çalıştırılacak kod')
+}
+
+//prompt() //Kullanıcı girişi için kullanılır 
+let age = prompt("Kaç yaşındasın ?")
+
+try {
+    if(age >= 18)
+    console.log("Yaş uygun")
+    else
+    throw alert("Siteyi kullanmak için uygun yaşta değilsiniz")
+} catch {
+  alert("18 yaşından büyük olmalısınız")
 }
