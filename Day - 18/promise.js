b8baf1d425399955f296a6a30c43610d722abda6
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-07 21:56:05 +0300

    async await

--- Day - 18/promise.js
+++ Day - 18/promise.js
@@ -13,7 +13,7 @@ const callback = (err, data) => {
 
 test(callback)*/
 
-const promise = new Promise((resolve, reject) => {
+/*const promise = new Promise((resolve, reject) => {
   //resolve("İşlem başarılı")
   reject("İşlem başarısız")
 })
@@ -22,5 +22,24 @@ promise.then((data) => {
   console.log(data)
 }).catch(err => {
   console.error(err)
-})
+})*/
 
+const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+/*fetch(API_URL)
+.then(res => res.json())
+.then(data => console.log(data)) 
+.catch(err => console.error(err))*/
+
+async function getPosts() {
+  try {
+  const response = await fetch(API_URL)
+  const data = await response.json()
+  console.log(data) 
+  } 
+  catch(err) {
+  console.error(err)
+    }
+  }
+
+  getPosts()
